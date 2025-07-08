7af98f18b38e1993973c5e59471df67472fdd143
ClaireStabile <claire.stabile@oig.dot.gov>
2025-07-08 17:13:48 -0400

    now with wonky maps - did not reproduce Grace's maps perfectly, will continue to work on that, but at least something is showing up on the dashboard

--- wv_dashboard.R
+++ wv_dashboard.R
@@ -8,8 +8,69 @@ library(stringdist)
 library(DT)
 library(progress)
 library(janitor)
+library(readxl)
+library(plotly)
+library(zipcodeR)
 
-#### demo data ####
+## Disable scientific notation
+options(scipen=999)
+
+
+#### data prep ####
+# load and clean data for maps 
+Data.Dir <- "o:\\Data_Analytics\\Support - Investigations\\Projects\\2025\\AUSA case dashboard\\WV data"
+WV.File  <- "RawWVdata.xlsx"
+
+WV.Data  <- file.path(Data.Dir, WV.File)
+WV.data  <- readxl::read_xlsx(WV.Data)
+
+case.File  <- "West Virgina Cases - approved after 05.01.2015.xlsx"
+case.Sheet <- "Sheet1"
+
+case.Data  <- file.path(Data.Dir, case.File)
+caseFed.Range <- "A2:E32"
+WV.casesFed <- read_excel(case.Data, sheet = case.Sheet, range = caseFed.Range)
+
+caseAlleg.Range <- "A35:E41"
+WV.casesAlleg <- read_excel(case.Data, sheet = case.Sheet, range = caseAlleg.Range)
+
+county.Sheet <- "CityCountyDis"
+county.file <- "West Virigina City, County, District.xlsx"
+county.Data  <- file.path(Data.Dir, county.file)
+districts <- read_excel(county.Data, sheet = county.Sheet)
+
+## Shortening Zip column to combine Zipcodes for analysis
+WV.data <- WV.data %>% mutate(fulZip = str_trim(Zip)) ## Trimming white space around zipcode so can pull out first five digits
+## Shortening Zipcode to only first five digits
+WV.data$Zipcode <- substr(WV.data$fulZip, 1, 5)
+
+total_table <- function(col) {
+  WV.data %>%
+    group_by({{col}}) %>%
+    summarise(
+      "Total Dollars" = sum(Amount, na.rm = TRUE),
+      "Total POs" = n_distinct(`PO#`)) %>%
+    arrange(desc(`Total Dollars`))
+}
+
+## Getting County FIPS codes
+json.counties <- jsonlite::fromJSON(url("https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"))
+county_fips_df <- json.counties$features %>%
+  transmute(
+    FIPS = id,
+    County = properties$NAME,
+    State = properties$STATE
+  ) %>%
+  filter(State == "54")
+
+# map counties onto AUSA districts
+county_districts <- left_join(districts, county_fips_df, by = "County")
+
+## Changing Alderson to 54075
+county_districts$FIPS[which(county_districts$`City/Town` == "Alderson")] <- "54075"
+
+
+# load and clean data for search table 
 wv_data_raw <- readxl::read_xlsx("O:/Data_Analytics/Support - Investigations/Projects/2025/AUSA case dashboard/WV data/RawWVdata.xlsx") %>%
   clean_names(); beep(2)
 
@@ -21,8 +82,7 @@ clean_data <- function(df) {
   df <- clean_names(df)
   df %>%
     mutate(across(where(is.character), str_trim)) %>%
-    filter(vendor_type != "EMPLOYEE") %>%
-    filter(pay_date >= as.Date("2024-01-01"))
+    filter(vendor_type != "EMPLOYEE") # remove employee info
 }; beep(2)
 
 # safely apply cleaning to all raw data with progress bar
@@ -49,6 +109,85 @@ state_map <- tibble(
   abbr = tolower(state.abb)
 )
 
+#### plot dataviz ####
+districtMap <- plot_ly() %>%
+  add_trace(
+    type="choroplethmapbox",
+    geojson=json.counties,
+    locations=county_districts$`FIPS`,
+    z=as.numeric(as.factor(county_districts$`AUSA District`)),
+    colorscale="Set1",
+    featureidkey="id",
+    text=~paste("County:", county_districts$County, "<br>AUSA District:", county_districts$`AUSA District`),
+    hovertemplate="%{text}<extra></extra>",
+    showscale = FALSE
+  ) %>% 
+  layout(title = "West Virginia Counties by AUSA District", showlegend = FALSE,
+         mapbox=list(
+           style="carto-positron",
+           zoom = 6,
+           center = list(lon = -80.5, lat = 39)) # Centering on West Virginia
+  )
+
+districtMap
+
+By.Zip <- total_table(Zipcode)
+zip_info <- zipcodeR::reverse_zipcode(By.Zip$Zipcode)
+zipCounty <- zip_info %>% select(zipcode,county,state)
+zipCountyDols <- left_join(By.Zip, zipCounty, by = c("Zipcode" = "zipcode"))
+
+## table of NA counties
+zipCountyDols %>% filter(is.na(county) | state != "WV")
+
+## Adding in County based on city listed in WV.data to incorrect Zipcodes in above table
+zipCountyDols$county[which(zipCountyDols$Zipcode == "23305")] <- "Kanawha"
+zipCountyDols$county[which(zipCountyDols$Zipcode == "32256")] <- "Monongalia"
+zipCountyDols$county[which(zipCountyDols$Zipcode == "83110")] <- "Upshur"
+
+## Removing work "County" from county names
+zipCountyDols$County <- gsub(" County", "", zipCountyDols$county, ignore.case = TRUE)
+
+## consolidating county_district data
+countyBydistrict <- county_districts %>%
+  select(County, `AUSA District`, FIPS) %>%
+  distinct()
+
+## Combining datasets by counties
+County_dolls <- full_join(zipCountyDols, countyBydistrict, by = "County")
+
+## Grouping Dollars and POs by County
+By.County <- County_dolls %>% 
+  group_by(County,FIPS,`AUSA District`) %>%
+  summarise(
+    "Total Dollars" = sum(`Total Dollars`, na.rm = TRUE),
+    "Total POs" = sum(`Total POs`, na.rm = TRUE)) %>%
+  arrange(desc(`Total Dollars`))
+
+## Removing "Pocahontas/Greenbrier" from By.County
+By.County <- By.County %>%
+  filter(County != "Pocahontas;Greenbrier")
+
+## Plotting Dollars by County
+countyDolMap <- plot_ly() %>% 
+  add_trace(
+    type="choroplethmapbox",
+    geojson=json.counties,
+    locations=By.County$FIPS,
+    z=as.numeric(as.factor(By.County$`Total Dollars`)),
+    colorscale="Viridis",
+    featureidkey="id",
+    text=~paste("County:", By.County$County, "<br>AUSA District:", By.County$`AUSA District`, 
+                "<br>Total Dollars: $", formatC(By.County$`Total Dollars`, format = "f", big.mark = ",", digits = 2),"<br>Total POs: ", By.County$`Total POs`),
+    hovertemplate="%{text}<extra></extra>"
+  ) %>% 
+  layout(title = "West Virginia Counties by DOT Spending",
+         mapbox=list(
+           style="carto-positron",
+           zoom = 6,
+           center = list(lon = -80.5, lat = 39))) # Centering on West Virginia
+
+countyDolMap
+
 #### ui layout ####
 ui <- navbarPage(
   id = "wv_portal",
@@ -58,12 +197,12 @@ ui <- navbarPage(
              titlePanel("West Virginia Dashboard"),
              fluidRow(
                column(6,
-                      tags$img(src = "image1.png", height = "300px", width = "100%"),
+                      plotly::plotlyOutput("countyDolMap", height = "300px"),
                       actionButton("go_nowhere", "Coming Soon")
                ),
                column(6,
                       tags$img(src = "image2.png", height = "300px", width = "100%"),
-                      actionButton("go_search", "Go to Search")
+                      actionButton("go_search", "search data")
                )
              )
            )
@@ -114,6 +253,11 @@ server <- function(input, output, session) {
     search_results(NULL)
   })
   
+  # render county dollars map
+  output$countyDolMap <- renderPlotly({
+    countyDolMap
+  })
+  
   searching <- reactiveVal(FALSE)
   search_results <- reactiveVal(NULL)
   
@@ -132,9 +276,9 @@ server <- function(input, output, session) {
       contains <- str_detect(vec_clean, fixed(q_lower))
       
       if (local_filter == "oa") {
-        acronyms <- vec_clean |>
-          str_replace_all("[^a-z0-9\\s]", "") |>
-          str_split("\\s+") |>
+        acronyms <- vec_clean |> 
+          str_replace_all("[^a-z0-9\\s]", "") |> 
+          str_split("\\s+") |> 
           map_chr(~ paste0(substr(.x[!tolower(.x) %in% c("of","the","and","for","to")],1,1), collapse = ""))
         exact_acr <- which(acronyms == q_lower)
         dist_acr <- stringdist::stringdist(q_lower, acronyms, method = "jw")
